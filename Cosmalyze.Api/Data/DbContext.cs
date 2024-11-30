78a77d6fc727efae43b21d381c2e3ea050127852
gurkankizik <gurkankizik@hotmail.com>
2024-11-30 21:08:59 +0300

    DbContext Path Fix

--- Cosmalyze.Api/Data/DbContext.cs
+++ Cosmalyze.Api/Data/DbContext.cs
@@ -1,5 +1,4 @@
-﻿using Cosmalyze.Api.Helpers;
-using Cosmalyze.Api.Models;
+﻿using Cosmalyze.Api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Cosmalyze.Api.Data
@@ -22,19 +21,19 @@ namespace Cosmalyze.Api.Data
 
         private void SeedBrands(ModelBuilder modelBuilder)
         {
-            var brands = JsonHelper.ReadBrandsFromJson("C:/Users/Gürkan/source/repos/Cosmalyze.Api/Cosmalyze.Api/Brands.json");
+            //var brands = JsonHelper.ReadBrandsFromJson("C:/Users/Gürkan/source/repos/Cosmalyze.Api/Cosmalyze.Api/Brands.json");
 
-            foreach (var brand in brands)
-            {
-                modelBuilder.Entity<Brand>().HasData(brand);
-            }
+            //foreach (var brand in brands)
+            //{
+            //    modelBuilder.Entity<Brand>().HasData(brand);
+            //}
 
-            var products = JsonHelper.ReadProductsFromJson("C:/Users/Gürkan/source/repos/Cosmalyze.Api/Cosmalyze.Api/Products.json");
+            //var products = JsonHelper.ReadProductsFromJson("C:/Users/Gürkan/source/repos/Cosmalyze.Api/Cosmalyze.Api/Products.json");
 
-            foreach (var product in products)
-            {
-                modelBuilder.Entity<Product>().HasData(product);
-            }
+            //foreach (var product in products)
+            //{
+            //    modelBuilder.Entity<Product>().HasData(product);
+            //}
         }
     }
 }
